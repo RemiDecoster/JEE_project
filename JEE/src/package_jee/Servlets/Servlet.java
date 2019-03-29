2edab8a47db126cd0f299d08261f3d7762f7f5e1
jotto <jotto@enssat.fr>
2019-03-29 17:05:38 +0100

    classe users (admin, editeur)

---
+++ JEE/src/package_jee/Servlets/Servlet.java
@@ -0,0 +1,20 @@
+package package_jee;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class Servlet extends HttpServlet {
+	private void doProcess( HttpServletRequest request, HttpServletResponse response) {
+		String pageName = "/accueil.jsp";
+		RequestDispatcher rd = getServletContext().getRequestDispatcher(pageName);
+		try{rd.forward(request, response);} 
+		catch(ServletException e) {e.printStackTrace();} 
+		catch(IOException e) {e.printStackTrace();}
+
+	}
+}
---
+++ JEE/src/package_jee/users/Users.java
@@ -0,0 +1,34 @@
+package users;
+
+public class User {
+	public User() {
+		super();
+	}
+	
+	private String pseudo;
+	private boolean isAdmin;
+	private boolean isEditor;
+	
+	public String getPseudo() {
+		return pseudo;
+	}
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+	public boolean isAdmin() {
+		return isAdmin;
+	}
+	public void setAdmin(boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+	public boolean isEditor() {
+		return isEditor;
+	}
+	public void setEditor(boolean isEditor) {
+		this.isEditor = isEditor;
+	}
+	public String toString() {
+		return pseudo + " Admin : "+isAdmin+ ", Editeur : "+ isEditor;
+	}
+	
+}
